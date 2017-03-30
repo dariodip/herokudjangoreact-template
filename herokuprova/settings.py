d9971cf1e1ba24e0879e5d8d67aced8cd32c7be7
dario <darionovantadue@gmail.com>
2017-03-30 09:46:38 +0200

    changed db to postgres

--- herokuprova/settings.py
+++ herokuprova/settings.py
@@ -80,10 +80,12 @@ WSGI_APPLICATION = 'herokuprova.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
     }
 }
+
+DATABASES['default'] =  dj_database_url.config()
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
