ada54d9090f9bcd82df5ac6b5b8357ee01a07781
darioSkal <darioskal@users.noreply.github.com>
2017-03-31 15:43:09 +0200

    Update settings.py

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -22,7 +22,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "=uq=q3cp(k3chy!^yw#(1vp7me+$7(ew#d1o^yk83uxk%0a9qy"
+SECRET_KEY =  os.environ['SECRET_VARIABLE']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
